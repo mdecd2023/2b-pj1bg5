75d791cd02d763a8e32458ae01a27286dd8ed88f
gu811 <41023219@gm.nfu.edu.tw>
2023-03-13 17:30:24 +0800

    ssh

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': 'cmsimde_site \n 組員:陳冠佑41023219，陳奕倫41023222', 'tags': '', 'url': 'About.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': 'cmsimde_site \n 組員:陳冠佑41023219，陳奕倫41023222 \n', 'tags': '', 'url': 'About.html'}, {'title': '2b-pj1bg5', 'text': '', 'tags': '', 'url': '2b-pj1bg5.html'}]};
