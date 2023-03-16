dc5bff2831e5545aa5d831bc7d7f15863978cba8
gu811 <41023219@gm.nfu.edu.tw>
2023-03-16 16:38:14 +0800

    新增網站

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': '2b-pj1bg5 \n 倉儲: https://github.com/mdecd2023/2b-pj1bg5 \n 組員:陳冠佑41023219，陳奕倫41023222 \n', 'tags': '', 'url': 'About.html'}, {'title': 'tutorial 1', 'text': '', 'tags': '', 'url': 'tutorial 1.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': '2b-pj1bg5 \n 倉儲: https://github.com/mdecd2023/2b-pj1bg5 \n 網站: https://mdecd2023.github.io/2b-pj1bg5 \n 組員:陳冠佑41023219，陳奕倫41023222 \n', 'tags': '', 'url': 'About.html'}, {'title': 'tutorial 1', 'text': '', 'tags': '', 'url': 'tutorial 1.html'}]};
