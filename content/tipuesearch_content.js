ccde99c74f28c8137f05c47036a26224a137e168
gu811 <41023219@gm.nfu.edu.tw>
2023-03-16 16:36:26 +0800

    edit1

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': '', 'tags': '', 'url': 'About.html'}, {'title': '2b-pj1bg5', 'text': '\n 倉儲: https://github.com/mdecd2023/2b-pj1bg5 \n 組員:陳冠佑41023219，陳奕倫41023222 \n', 'tags': '', 'url': '2b-pj1bg5.html'}, {'title': 'tutorial 1', 'text': '', 'tags': '', 'url': 'tutorial 1.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': '2b-pj1bg5 \n 倉儲: https://github.com/mdecd2023/2b-pj1bg5 \n 組員:陳冠佑41023219，陳奕倫41023222 \n', 'tags': '', 'url': 'About.html'}, {'title': 'tutorial 1', 'text': '', 'tags': '', 'url': 'tutorial 1.html'}]};
